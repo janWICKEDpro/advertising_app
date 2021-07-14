b97e6205f6858cd6dce7aa37268ce444b0046e6c
janWICKEDpro <janroyal45@gmail.com>
2021-07-14 02:11:12 +0100

    stuff

--- lib/authenticationMethods/authentication.dart
+++ lib/authenticationMethods/authentication.dart
@@ -6,7 +6,8 @@ class AuthService{
   //sign in with email and password
   Future signInWithEmailPassword(String email, String password)async{
       try{
-        final user = await auth.signInWithEmailAndPassword(email: email, password: password);
+        final result = await auth.signInWithEmailAndPassword(email: email, password: password);
+        User? user = result.user;
         return user;
       }catch(e){
           print(e);
@@ -15,6 +16,14 @@ class AuthService{
 
 
   //register with email and password
+  Future registerEmailandPassword() async{
+    try{
+     // final user = await auth.R
+    }catch(e){
+      print(3);
+      return null;
+    }
+  }
 
   //sign out 
 
