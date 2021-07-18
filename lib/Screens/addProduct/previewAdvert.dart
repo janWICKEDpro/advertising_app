f5b45b7dac61e8575c9da167ad7e057d01a92cef
janWICKEDpro <janroyal45@gmail.com>
2021-07-18 03:12:41 +0100

    preview advert in progress

---
+++ lib/Screens/addProduct/previewAdvert.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PreviewAdvert extends StatefulWidget {
+  const PreviewAdvert({ Key? key }) : super(key: key);
+
+  @override
+  _PreviewAdvertState createState() => _PreviewAdvertState();
+}
+
+class _PreviewAdvertState extends State<PreviewAdvert> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
