ae2d94037fd70bb70098b5470d31e51610fddd39
janWICKEDpro <janroyal45@gmail.com>
2021-08-03 02:11:31 +0100

    project restarted

--- lib/objects/userObject.dart
+++ lib/objects/userObject.dart
@@ -3,7 +3,8 @@ class User {
   String? username;
   String? id;
   String? email;
+  int? contact;
 
-  User({this.username,this.email,this.id});
+  User({this.username,this.email,this.id, this.contact});
 }
 
