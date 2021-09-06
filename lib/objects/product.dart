645f5140268be8b5494661850979e006bfa0b11a
janWICKEDpro <janroyal45@gmail.com>
2021-09-06 12:19:16 +0100

    product object

---
+++ lib/objects/product.dart
@@ -0,0 +1,13 @@
+import 'package:advertise/enums/categories.dart';
+
+class Product{
+String? name;
+int? price;
+String? title;
+String? description;
+String? link;
+int? contact;
+DateTime? date;
+categories? category;
+
+}
