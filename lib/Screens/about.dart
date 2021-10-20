16e245294e4801b87c5ba5baf7ab24bf7a1de9be
janWICKEDpro <janroyal45@gmail.com>
2021-10-20 17:25:54 +0100

    nothing

--- lib/Screens/about.dart
+++ lib/Screens/about.dart
@@ -10,7 +10,7 @@ class About extends StatelessWidget {
       body: Container(
         child: Center(
           child: Card(
-            child: Text("This is an app invented by Jan"),
+            child: Text("h"),
           ),
         ),
       ),
